c7875f3452d77ca216ad7ddf53b3f752c6b9fe59
Eliezir Moreira <eliezir2004@gmail.com>
2022-08-19 11:47:33 -0300

    Update index.js

--- index.js
+++ index.js
@@ -8,64 +8,74 @@ var activeCol = 0;
 
 function CreateGame(modo){
 for (let i = 0; i < modo; i-=-1){
-    var gameSet = i;    
-    games[i] = document.createElement("div");
+var gameSet = i;    
+games[i] = document.createElement("div");
 for(let rowIndex = 0; rowIndex < rows; rowIndex++) {
-    const tileRow = document.createElement("div");
-    tileRow.setAttribute("id","game"+gameSet+"row" + rowIndex)
-    for(let columnIndex = 0; columnIndex < columns; columnIndex++) {
-        const tileColumn = document.createElement("div");
-        tileColumn.setAttribute("id","game"+gameSet+"row"+rowIndex+"column"+columnIndex);
-        tileColumn.setAttribute("class","tile-column");
-        tileRow.append(tileColumn);
-    }
-    games[i].append(tileRow);
-    tiles.append(games[i]);
+const tileRow = document.createElement("div");
+tileRow.setAttribute("id","game"+gameSet+"row" + rowIndex)
+for(let columnIndex = 0; columnIndex < columns; columnIndex++) {
+    const tileColumn = document.createElement("div");
+    tileColumn.setAttribute("id","game"+gameSet+"row"+rowIndex+"column"+columnIndex);
+    tileColumn.setAttribute("class","tile-column");
+    tileRow.append(tileColumn);
+}
+games[i].append(tileRow);
+tiles.append(games[i]);
 }}}
 CreateGame(1);
 
-    for(let col = 0; col < columns; col ++){
-        var active_columns = document.getElementById("game0row0column"+col)
-        active_columns.classList.toggle("typing")
+for(let col = 0; col < columns; col ++){
+    var active_columns = document.getElementById("game0row0column"+col)
+    active_columns.classList.toggle("typing")
 }
 
 
 function letterFocus(x){
-    activeRow = document.querySelectorAll(".typing");
-    activeRow[x].classList.toggle("letter-focus")
+activeRow = document.querySelectorAll(".typing");
+activeRow[x].classList.toggle("letter-focus")
 }
 
 letterFocus(activeCol);
 
 
 document.addEventListener("keydown", function (event) {
-    var emptyTiles = activeRow.length;
-    console.log(emptyTiles);
-    switch(event.key){
-        case "ArrowRight":
-        letterFocus(activeCol);
-        activeCol++;
-        if(activeCol>emptyTiles-1)activeCol=0;   
-        letterFocus(activeCol);
-        break;
-        case "ArrowLeft":
-        letterFocus(activeCol);
-        activeCol--;
-        if(activeCol<0)activeCol = emptyTiles-1;
-        letterFocus(activeCol);
-        break;
-    }
-  
+var emptyTiles = activeRow.length;
+switch(event.key){
+    case "ArrowRight":
+    letterFocus(activeCol);
+    activeCol++;
+    if(activeCol>emptyTiles-1)activeCol=0;   
+    letterFocus(activeCol);
+    break;
+    case "ArrowLeft":
+    letterFocus(activeCol);
+    activeCol--;
+    if(activeCol<0)activeCol = emptyTiles-1;
+    letterFocus(activeCol);
+    break;
+}
+
 });
 
-for(var index in activeRow){
-    var collumnIndex = activeRow[index];
-    console.log(activeRow[index])
-    /* collumnIndex.addEventListener("click",function(event){
-    console.log(event)
-    }) */
-    document.getElementById("game0row0column4").addEventListener("click",function(event){
-        console.log(event)
-    })
+
+function changeEventListener(type){
+activeRow.forEach(cel =>{
+cel.addEventListener("click",function(e){
+    var removeFocus = document.getElementsByClassName("letter-focus")
+    if(removeFocus[0].id == this.id){}
+    else{
+        console.log(removeFocus[0])
+        removeFocus[0].classList.remove("letter-focus");
+        document.getElementById(this.id).classList.add("letter-focus");
+        console.log(activeRow.findIndex(myFunction))
+        function myFunction(){
+            return this;
+        }
+    }
+})
+})
 }
+
+changeEventListener("teste");
+
 
