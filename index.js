613229c3184e443f757e3db20d7458be9e1680d2
Eliezir Moreira <eliezir2004@gmail.com>
2022-08-20 23:51:54 -0300

    inserindo e removendo texto e enviando palavra

--- index.js
+++ index.js
@@ -3,16 +3,20 @@ const games = []
 
 const rows = 6;
 const columns = 5;
-var activeRow = 0;
+var activeRowIndex = 0;
 var activeCol = 0;
 
+
+
+/* Cria o tabuleiro para jogo */
 function CreateGame(modo){
 for (let i = 0; i < modo; i-=-1){
 var gameSet = i;    
 games[i] = document.createElement("div");
 for(let rowIndex = 0; rowIndex < rows; rowIndex++) {
 const tileRow = document.createElement("div");
-tileRow.setAttribute("id","game"+gameSet+"row" + rowIndex)
+tileRow.setAttribute("id","game"+gameSet+"row" + rowIndex);
+tileRow.setAttribute("class","tile-row");
 for(let columnIndex = 0; columnIndex < columns; columnIndex++) {
     const tileColumn = document.createElement("div");
     tileColumn.setAttribute("id","game"+gameSet+"row"+rowIndex+"column"+columnIndex);
@@ -21,61 +25,129 @@ for(let columnIndex = 0; columnIndex < columns; columnIndex++) {
 }
 games[i].append(tileRow);
 tiles.append(games[i]);
+
 }}}
+
 CreateGame(1);
 
+
+/* define a linha da rodada */
+function setRow(row){
 for(let col = 0; col < columns; col ++){
-    var active_columns = document.getElementById("game0row0column"+col)
+    var active_columns = document.getElementById("game0row"+row+"column"+col)
     active_columns.classList.toggle("typing")
-}
+}}
 
-
+/* define a tecla em foco */
 function letterFocus(x){
+if(x > -1){  
 activeRow = document.querySelectorAll(".typing");
-activeRow[x].classList.toggle("letter-focus")
+activeRow[x].classList.toggle("letter-focus")}
 }
 
-letterFocus(activeCol);
 
 
+
+function changeKey(type){
+    var emptyTiles = activeRow.length;
+    if(activeCol > -1)letterFocus(activeCol);
+    if(type =="backward"){
+    activeCol--;
+    if(activeCol<0)activeCol = emptyTiles-1;}
+    else if(type =="forward"){
+        activeCol++;
+        if(activeCol>emptyTiles-1)activeCol=0;   
+    }
+    letterFocus(activeCol);
+}
+
+/* event listeners do teclado*/
 document.addEventListener("keydown", function (event) {
-var emptyTiles = activeRow.length;
+   /*  mover entre as teclas com o teclado  */
+
 switch(event.key){
-    case "ArrowRight":
-    letterFocus(activeCol);
-    activeCol++;
-    if(activeCol>emptyTiles-1)activeCol=0;   
-    letterFocus(activeCol);
+    case "ArrowRight":   
+        changeKey("forward")
     break;
     case "ArrowLeft":
-    letterFocus(activeCol);
-    activeCol--;
-    if(activeCol<0)activeCol = emptyTiles-1;
-    letterFocus(activeCol);
+        changeKey("backward")
     break;
+    case "Backspace":
+    if(activeCol ==-1){
+        activeRow[4].classList.toggle("letter-focus")
+        activeCol = 4;
+    }
+    var activeCell = document.getElementsByClassName("letter-focus");
+    activeCell[0].innerHTML ="";
+    break;
+    case "Enter":
+    var palavra = "";
+    for(var x = 0 ; x < columns ; x++){
+        palavra+= activeRow[x].innerHTML;
+    }
+    /* aqui tem q checar se a palavra está certa, se ela existe pra caso seja errada rodar o if  */
+    if(palavra.length == 5)
+    {
+        console.log(palavra)
+        letterFocus(activeCol);
+        setRow(activeRowIndex);
+        activeCol = 0;
+        changeEventListener("remove");
+        activeRowIndex ++;
+        setRow(activeRowIndex);
+        letterFocus(activeCol);
+        changeEventListener("add");
+    }
+
+    break;
+
+}
+
+var key = event.key.toUpperCase();
+if( key.length == 1 && key.charCodeAt() >=65 &&  key.charCodeAt()  <= 90 ){
+    var filled = 0;
+    var activeCell = document.getElementsByClassName("letter-focus");
+    activeCell[0].innerHTML =key;
+    changeKey("forward");
+    if(activeCell[0].innerHTML != ""){
+        while(activeCell[0].innerHTML != "" && filled < 4){
+            filled ++;
+            changeKey("forward")
+        }
+        if(filled == 4){
+            letterFocus(activeCol);
+            activeCol = -1;
+        }
+    }
 }
 
 });
 
-
+/* event listeners para mover entre as teclas com o mouse */
 function changeEventListener(type){
 activeRow.forEach(cel =>{
-cel.addEventListener("click",function(e){
+if(type == "add"){
+cel.addEventListener("click",function handleClick(e){
     var removeFocus = document.getElementsByClassName("letter-focus")
-    if(removeFocus[0].id == this.id){}
-    else{
-        console.log(removeFocus[0])
-        removeFocus[0].classList.remove("letter-focus");
-        document.getElementById(this.id).classList.add("letter-focus");
-        console.log(activeRow.findIndex(myFunction))
-        function myFunction(){
-            return this;
-        }
+        if(removeFocus[0].id == this.id){}
+        else    {
+            removeFocus[0].classList.remove("letter-focus");
+            document.getElementById(this.id).classList.add("letter-focus");
+            var name = this.id;
+            name = name.slice(-1)
+            activeCol = parseInt(name)
+                }
+        });
     }
-})
+else if(type =="remove")
+    cel.replaceWith(cel.cloneNode(true));
+
 })
 }
 
-changeEventListener("teste");
 
 
+
+setRow(activeRowIndex);
+letterFocus(activeCol);
+changeEventListener("add");
